27d19bde4af85eb15487e6bbe5d6672bcfc8cf4e
unknown <zhuangwei@ZHUANGWEI001.szvdi.ys100.cloud.com>
2018-11-16 19:15:35 +0800

    提交轮播

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,55 +2,18 @@
 //获取应用实例
 const app = getApp()
 
-Page({
-  data: {
-    name: '',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log(app.globalData);
-    if (app.globalData.userInfo) {
-      this.setData({
-        name: app.globalData.userInfo.nickName,
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  Page({
+    data: {
+      imgUrls: [
+        '../../imgs/cs1.jpg',
+        '../../imgs/cs2.jpg',
+        '../../imgs/cs3.jpg',
+        '../../imgs/cs4.jpg'
+      ],
+      indicatorDots: false,
+      autoplay: true,
+      interval: 3000,
+      duration: 1000,
+      circular:true
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+  })
