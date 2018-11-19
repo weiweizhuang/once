45ab839e44b746b6da32cc63b73da75ba5692f01
weiweizhuang <iwei.zhuang@outlook.com>
2018-11-19 14:18:38 +0800

    首页布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,31 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var order = ['red', 'yellow', 'blue', 'green', 'red']
   Page({
     data: {
-      imgUrls: [
-        '../../imgs/cs1.jpg',
-        '../../imgs/cs2.jpg',
-        '../../imgs/cs3.jpg',
-        '../../imgs/cs4.jpg'
-      ],
-      indicatorDots: false,
-      autoplay: true,
-      interval: 3000,
-      duration: 1000,
-      circular:true
-    }
+        toView: 'red',
+        scrollTop: 100,
+        imgUrls: [
+            '../../imgs/cs1.jpg',
+            '../../imgs/cs2.jpg',
+            '../../imgs/cs3.jpg',
+            '../../imgs/cs4.jpg'
+        ],
+        indicatorDots: false,
+        autoplay: true,
+        interval: 3000,
+        duration: 1000,
+        circular:true
+    },
+    tap: function (e) {
+        for (var i = 0; i < order.length; ++i) {
+            if (order[i] === this.data.toView) {
+                this.setData({
+                    toView: order[i + 1]
+                })
+                break
+            }
+        }
+    },
   })
